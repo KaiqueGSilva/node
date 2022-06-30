85bf4749f46b37597bdb8c2fb55f45231f42c0d7
KaiqueGSilva <kaka.gon.silva@gmail.com>
2022-06-30 14:20:28 -0300

    atualizar arquivo

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router, Request, Response} from 'express'
+
+const router = Router()
+
+// CRIAR A PÁGINA HOME, CONTATO E SOBRE
+
+router.get('/',(req:Request, res:Response) =>{
+    res.render('home')
+})
+
+router.get('/contato',(req:Request, res:Response) =>{
+    res.render('contato')
+})
+
+router.get('/sobre',(req:Request, res:Response) =>{
+    res.render('sobre')
+})
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,30 @@
 import express, {Request, Response} from 'express'
-import { Server } from 'http'
+// import { Server } from 'http'
+import mainRoutes from './routes/index'
+import path from 'path'
+
+import mustache from 'mustache-express'
 
 //Usando express
 const server = express()
 
-server.get('/',(req:Request,res:Response) =>{
+//CONFIGURANDO O MUSTACHE
+server.set('view engine', 'mustache')
 
-    res.send("Doente de amor procurei remedio na vida noturna")
+//CONFIGURANDO A PASTA VIEWS PARA O MUSTACHE
+server.set('views',(path.join(__dirname,'views')))
 
+server.engine('mustache',mustache())
+
+//Criando a rota para acessar a pasta public
+server.use(express.static(path.join(__dirname,'../public')))
+
+//INSERINDO A ROTA NO SERVIDOR
+server.use(mainRoutes)
+
+server.use((req:Request, res:Response) =>{
+    res.status(404).send("Página não encontrada")
 })
 
+//ESCUTANDO A PORTA 3000
 server.listen(3000)
